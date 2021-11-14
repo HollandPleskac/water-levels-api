8ce2526d525f186c3e63276f9d4e14b7a1a405a4
Holland Pleskac <hollandpleskac@gmail.com>
2021-11-14 13:51:32 -0800

    comment out

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const fs = require('fs').promises;
-
 const oroville_lake_json = require('./json_data/oroville_lake.json')
 const trinity_lake_json = require('./json_data/trinity_lake.json')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 
 app.get('/', (req, res) => {
