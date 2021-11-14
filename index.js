3197c146fb7d889da5c8d0cf141d8c426c980e6c
Holland Pleskac <hollandpleskac@gmail.com>
2021-11-14 14:01:39 -0800

    comment in

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 
 app.get('/', (req, res) => {
